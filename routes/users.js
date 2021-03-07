44bb8136beebaabf71b2bfd581482bbfacf61e95
Ritesh_tikare <60392084+riteshtikare@users.noreply.github.com>
2021-03-07 09:12:22 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-mongoose.connect('mongodb+srv://ritesh:Ritesh@123@cluster0.tkdvm.mongodb.net/ritesh?retryWrites=true&w=majority');
+mongoose.connect("mongodb+srv://ritesh_16:ritesh16@cluster0.tkdvm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
 var userSchema = mongoose.Schema({
     name: String,
